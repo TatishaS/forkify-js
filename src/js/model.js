e6c8e767fcd75da578de0094156fb0afc0e1aacd
Tatiana <snow_white_girl@bk.ru>
2021-06-09 16:57:23 +0300

    Fixed bug in displaying page number in search results

--- src/js/model.js
+++ src/js/model.js
@@ -38,7 +38,6 @@ export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
     const data = await getJSON(`${API_URL}?search=${query}`);
-    console.log(data);
 
     state.search.results = data.data.recipes.map(rec => {
       return {
@@ -48,7 +47,7 @@ export const loadSearchResults = async function (query) {
         image: rec.image_url,
       };
     });
-    console.log(state.search.results);
+    state.search.page = 1;
   } catch (err) {
     console.error(`${err} 💣`);
     throw err;
