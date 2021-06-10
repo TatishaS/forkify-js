fd95a3eb85e556ad910e73e62efe6d8a6bde54ac
Tatiana <snow_white_girl@bk.ru>
2021-06-10 12:07:58 +0300

    Added storage of bookmarks in localStorage

--- src/js/controller.js
+++ src/js/controller.js
@@ -22,16 +22,19 @@ const controlRecipes = async function () {
 
     // 0. Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+
+    // 1. Update bookmarks view
     bookmarksView.update(model.state.bookmarks);
 
-    // 1. Loading a recipe
+    // 2. Loading a recipe
     await model.loadRecipe(id);
 
-    // 2. Rendering recipe
+    // 3. Rendering recipe
 
     recipeView.render(model.state.recipe);
   } catch (err) {
     recipeView.renderError();
+    console.error(err);
   }
 };
 
@@ -80,7 +83,13 @@ const controlAddBookmark = function () {
   // 3) Render bookmarks
   bookmarksView.render(model.state.bookmarks);
 };
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
