10472f7b281d33c29bba8d5895ba1f0ecb607089
Tatiana <snow_white_girl@bk.ru>
2021-06-10 17:12:47 +0300

    Added functional for uploading and saving own recipes

--- src/js/config.js
+++ src/js/config.js
@@ -2,3 +2,5 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'bcce697e-1b6d-4883-9e03-62112b7977eb';
+const MODAL_CLOSE_SEC = 2.5;
